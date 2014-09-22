88d10059698e90ebaddc057448869f02056dd164
Laxminath Kasam <lkasam@codeaurora.org>
2014-09-22 15:57:08 +0530

    ASoC: msm: reorg q6core header file
    This is needed to allow include from
    non soc folder.
    
    CRs-Fixed: 689849
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/msm8226.c
    
    Change-Id: I8aa438b50c696c674e82474ec53a88454f34ce37

--- sound/soc/msm/msm8226.c
+++ sound/soc/msm/msm8226.c
@@ -31,7 +31,7 @@
 #include <mach/socinfo.h>
 #include <mach/subsystem_notif.h>
 #include "qdsp6v2/msm-pcm-routing-v2.h"
-#include "qdsp6v2/q6core.h"
+#include <sound/q6core.h>
 #include "../codecs/wcd9xxx-common.h"
 #include "../codecs/wcd9306.h"
 #ifdef CONFIG_LGE_STEREO_SPEAKER
